f3751cf62320a9832bda9ca999f9fee973350073
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-06-25 23:34:51 -0400

    finished the first problem with 2 solutions

--- chapter1/1-1.rb
+++ chapter1/1-1.rb
@@ -1,2 +1,41 @@
 # Determine if a string has all unique characters. What if you cannot use additional data structures?
+# s = "paloemjk"
+def unique (string)
+    i = 0
+    while i < string.length
+        j = i + 1
+        while j < string.length + 1
+            if string[i] == string[j]
+                puts "not unique"
+                return false
+            else
+                j += 1
+            end
+        end
+        i += 1
+    end
+    puts "unique"
+    return true
+end
 
+# Linear and more effective solution, time complexity will be O(n) while in the previous example it was O(n*n)
+
+def unique (string)
+    i = 0
+    hashes = Hash.new(false)
+    while i < string.length
+        value = string[i]
+        puts value
+        if hashes[value] == true
+            puts "not unique"
+            return false
+        else
+            hashes[value] = true
+        end
+        i += 1
+    end
+    puts "unique"
+    return true
+end
+
+# unique(s)
