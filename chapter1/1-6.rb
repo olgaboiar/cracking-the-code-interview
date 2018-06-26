f48665d867e94ffe41cdb8ccaeaa9ddda85b8430
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-06-25 21:30:32 -0400

    startet 1-1 and 1-6 problems

---
+++ chapter1/1-6.rb
@@ -0,0 +1,13 @@
+string = "aabcccccaaa"
+def compression (string)
+    str_len = string.length
+    if str_len <= 2
+        return string
+    else
+        count = 1
+        result = ""
+        current = string[0]
+        puts current
+    end
+end
+compression(string)
