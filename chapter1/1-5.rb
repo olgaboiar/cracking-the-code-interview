6e7ef2f2917ccc8a9b27039329ea7c5aaadbd6de
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-07-01 22:59:24 -0400

    complete dmy solution for the fifth problen

--- chapter1/1-5.rb
+++ chapter1/1-5.rb
@@ -1,7 +1,46 @@
 # One away
-string1 = ""
-stringn2 = ""
+string1 = "pame"
+string2 = "palme"
 def oneAway(string1, string2)
+    case string1.length - string2.length
+    when 0
+        count = 0
+        for i in (0..string1.length - 1)
+            if string1[i] != string2[i]
+                count += 1
+            end
+            i +=1
+        end
+        if count == 1
+            return true
+        else
+            return false
+        end
+    when 1
+        for i in (0..string1.length - 1)
+            if string1[i] != string2[i]
+                if string1[i+1] != string2[i]
+                    return false
+                else
+                    return true
+                end
+            end
+            i += 1
+        end
+    when -1
+        for i in (0..string2.length - 1)
+            if string2[i] != string1[i]
+                if string2[i+1] != string1[i]
+                    return false
+                else
+                    return true
+                end
+            end
+            i += 1
+        end
+    else
+        return false
+    end
 
 end
 oneAway(string1, string2)
---
+++ chapter1/test.rb
@@ -0,0 +1,13 @@
+def matrixElementsSum(matrix)
+    
+    # puts matrix[0][1]
+    # puts matrix[1]
+    # puts matrix[1][0]
+    puts matrix[1][1]
+    # puts matrix[1][1].class
+    # row = matrix[1]
+    # puts row[1]
+end
+matrix = [[0,1,1,2],[0,5,0,0],[2,0,3,3]]
+
+matrixElementsSum(matrix)
