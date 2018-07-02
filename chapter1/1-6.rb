294e26ad4910c8d9bd96353868bc114c49a84d4f
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-07-02 10:12:02 -0400

    completed the sixth problem

--- chapter1/1-6.rb
+++ chapter1/1-6.rb
@@ -1,3 +1,4 @@
+# String Compression
 string = "aabcccccaaa"
 def compression (string)
     str_len = string.length
@@ -5,9 +6,20 @@ def compression (string)
         return string
     else
         count = 1
-        result = ""
+        result = []
         current = string[0]
-        puts current
+        for i in (1..str_len)
+            if string[i] == current
+                count += 1
+            else
+                result.push(count)
+                result.push(current)
+                count = 1
+                current = string[i]
+            end
+            i += 1
+        end
+        return result.join("")
     end
 end
 compression(string)
