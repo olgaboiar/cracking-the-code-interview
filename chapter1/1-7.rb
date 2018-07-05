84fba9f5958da89313ae9e19f869bcf29a6df602
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-07-04 22:21:57 -0400

    started the seventh problem

--- chapter1/1-7.rb
+++ chapter1/1-7.rb
@@ -3,7 +3,7 @@ matrix = [[1,2,3],[4,5,6],[7,8,9]]
 def rotate(matrix)
     for i in (0..matrix.length)
         for k in (0..matrix[i].length)
-
+            #put logic here
         end
     end
 end
